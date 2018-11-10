74ed071c6ca2e2dcb696e00d39dedda626604ef8
joaocottabadaro <joaobadaro@gmail.com>
2018-11-10 16:19:36 -0200

    Add files via upload
    comentei

--- TrabalhoGrafos/Grafo.cpp
+++ TrabalhoGrafos/Grafo.cpp
@@ -3,15 +3,16 @@
 
 void Grafo::adcionarNo(int id, float pesoNo)
 {
-    No no = No();
+    No no = No();                               //adicionamos o no ao vetor dos vertices
     no.setId(id);
     listaAdj.push_back(no);
 }
 
 void Grafo::removerNo (int id)
 {
-    vector <No>::iterator it;
-    vector <Aresta>::iterator a;
+    vector <No>::iterator it;                   //Nessa funcao percorremos o a lista de nos no primeiro for ate achar o valor, e depois apagar ele
+    vector <Aresta>::iterator a;                //Nos dois proximos for queremos apagar a ligacao que o no deletado tinha com os outros , fazemos isso verificando se
+                                                //com o if se existe ligacao e pagamos
     int i=0,j=0;
     for(it = listaAdj.begin() ; it != listaAdj.end(); ++it, i++ ){
         if ( it->getId() == id ){
@@ -32,13 +33,13 @@ void Grafo::removerNo (int id)
 
 int Grafo::ordemGrafo()
 {
-    return listaAdj.size();
+    return listaAdj.size();                 //a ordem do grafo E a quantidade de nos, entao vai ser o tamanho da lista desses.
 }
 
 bool Grafo::grafoCompleto()
 {
-    int n = ordemGrafo();
-    int num_Arestas = 0;
+    int n = ordemGrafo();                       //para verificar se o grafo E completo precisamos percorrer o vetor de arestas verificando o
+    int num_Arestas = 0;                             //grau de cada no e verificar com o if se a soma de arestas da n*(n-1)
     for(int i = 0; i < n; i++) {
         num_Arestas += listaAdj[i].getGrau();
     }
@@ -50,8 +51,9 @@ bool Grafo::grafoCompleto()
 
 bool Grafo::grafoKRegularidade(int k)
 {
-    for (std::vector<No>::iterator it = listaAdj.begin(); it != listaAdj.end(); ++it) {
-                if(it->getGrau()!=k)
+    for (std::vector<No>::iterator it = listaAdj.begin(); it != listaAdj.end(); ++it) { //para um grafo de k regularidade os nos precisam ter o mesmo grau K
+                if(it->getGrau()!=k)                                                       // percorremos o vetor de nos verificando se algum nao tem K como grau
+                                                                                            //e retornar verdadeiro ou falso caso nao tenha
                 {
                     return false;
                 }
@@ -61,8 +63,8 @@ bool Grafo::grafoKRegularidade(int k)
 }
 
 void Grafo::vizinhacaAberta(int id)
-{
-    int i=0;
+{                                                                       //Para demonstrar a vizinhacaAberta so precisamos mostrar os nos que tem ligacao
+    int i=0;                                                            // com o no principal, fazemos isso percorrendo o vetor de arestas do no desejado
     for (std::vector<No>::iterator it = listaAdj.begin(); it != listaAdj.end(); ++it, i++) {
          if( it->getId() == id )
          {
@@ -75,7 +77,9 @@ void Grafo::vizinhacaAberta(int id)
 void Grafo::vizinhacaFechada(int id)
 {
     int i=0;
-    cout << id << endl;
+    cout << id << endl;                                         //Para demonstrar a vizinhacaFechada so precisamos mostrar os nos que tem ligacao
+    int i=0;                                                            // com o no principal, fazemos isso percorrendo o vetor de arestas do no desejado
+                                                                    //e acrescentar  o no principal
     for (std::vector<No>::iterator it = listaAdj.begin(); it != listaAdj.end(); ++it, i++) {
          if( it->getId() == id )
          {
@@ -107,7 +111,7 @@ bool Grafo::bipartido()
 
 void Grafo::sequenciaGraus()
 {
-    for (std::vector<No>::iterator it = listaAdj.begin(); it != listaAdj.end(); ++it) {
+    for (std::vector<No>::iterator it = listaAdj.begin(); it != listaAdj.end(); ++it) { //percorremos o vetor dos vertices e exibimos o grau deles
             cout << it->getGrau() << " ";
     }
 }
