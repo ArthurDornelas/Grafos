bd24bcb00d88ff08c08acb2255ed4886ffc0b866
Arthur Dornelas <arthurdornelas99@gmail.com>
2018-11-22 15:22:09 -0200

    Add files via upload

--- TrabalhoGrafos/Cluster.cpp
+++ TrabalhoGrafos/Cluster.cpp
@@ -1,6 +1,38 @@
-int Cluster::calculaDintra()
-{
+#include "Cluster.h"
+using namespace std
+#include <vector>
 
+
+float Cluster::calculaDintra(int idNo)
+{
+    float soma = 0;
+    int i = 0;
+    for(std::vector<No>::iterator no = noCluster.begin(); noCluster.end(); ++no){
+        if(no->getId() == idNo)
+            break;
+        i++;
+    }
+
+    for(std::vector<No>::iterator no = noCluster.begin(); noCluster.end(); ++no){
+
+        if(no->getId() != idNo)
+        {
+            for (std::vector<Aresta>::iterator aresta = arestasCluster.begin(); aresta != arestasCluster.end(); ++aresta)
+
+                if(aresta->getIdNo() != no->getId() || aresta->getIdLista() != no->getId()){
+                        soma += aresta->getPesoAresta();
+                }
+
+                else{
+                    soma += aresta->getPesoAresta();
+                    break;
+                }
+
+        }
+
+    }
+
+    cout<< soma << endl;
 
 
 
