4428441ecb4200976959f261e6e4b5183b706dfd
Arthur Dornelas <arthurdornelas99@gmail.com>
2018-11-24 15:26:35 -0200

    Add files via upload
    Elaborando D Intra

--- TrabalhoGrafos/Cluster.cpp
+++ TrabalhoGrafos/Cluster.cpp
@@ -3,34 +3,46 @@ using namespace std
 #include <vector>
 
 
-float Cluster::calculaDintra(int idNo)
+float Cluster::calculaDintra(int id)
 {
+    int id_lista = -1;
     float soma = 0;
     int i = 0;
     for(std::vector<No>::iterator no = noCluster.begin(); noCluster.end(); ++no){
-        if(no->getId() == idNo)
+        if(no->getId() == id)
             break;
         i++;
     }
 
-    for(std::vector<No>::iterator no = noCluster.begin(); noCluster.end(); ++no){
+    for(std::vector<No>::iterator no = noCluster.end(); noCluster.begin(); --no){
 
-        if(no->getId() != idNo)
+
+        if(no->getId() != id)
         {
-            for (std::vector<Aresta>::iterator aresta = arestasCluster.begin(); aresta != arestasCluster.end(); ++aresta)
+            for (std::vector<Aresta>::iterator aresta = arestasCluster.begin(); aresta != arestasCluster.end(); ++aresta){
 
-                if(aresta->getIdNo() != no->getId() || aresta->getIdLista() != no->getId()){
-                        soma += aresta->getPesoAresta();
+                if(aresta->getIdNo == no->getId()){
+                    id_lista = no->getId();
+
+                    for (std::vector<Aresta>::iterator arest = aresta; arest->getIdLista() != id ; ++aresta){
+
+                        if(aresta->getIdNo() != no->getId() || aresta->getIdLista() != no->getId()){
+                            soma += aresta->getPesoAresta();
+                        }
+
+                        else{
+                            soma += aresta->getPesoAresta();
+                            break;
+                        }
+                    }
+
                 }
-
-                else{
-                    soma += aresta->getPesoAresta();
-                    break;
-                }
-
+            }
         }
 
     }
+
+
 
     cout<< soma << endl;
 
